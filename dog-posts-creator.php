c8f3930d0b98a2b0c32163d10f4030c626675454
aleksioz <aleksa.mejl@gmail.com>
2025-03-24 14:17:06 +0100

    bump version to 1.0.4 and delete unnecessary code

--- dog-posts-creator.php
+++ dog-posts-creator.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Dog posts creator
  * Description: A plugin to handle creating "Dog" and editing posts based on Forminator form data.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: aleksioz
  * https://github.com/aleksioz
  * License: GPL2
@@ -17,8 +17,6 @@ define( 'DPC_PATH', plugin_dir_path( __FILE__ ) );
 
 // Include the required files
 require_once DPC_PATH . 'includes/class-init.php';
-
-use DogPostsCreator\Init;
 
 // Initialize the plugin
 add_action( 'plugins_loaded', ['DogPostsCreator\Init', 'instance' ]);
